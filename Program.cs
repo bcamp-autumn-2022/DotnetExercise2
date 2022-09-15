3f4408b94be8b313c151272d4d12b62ca0916449
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-09-15 11:52:31 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace DotnetExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url;
+            Console.WriteLine("Give the url");
+            url=Console.ReadLine();
+            string test = url.Substring(0, 8);
+            if (String.Compare(test, "https://") == 0)
+            {
+                
+            }
+            else {
+                url = "https://" + url;
+            }
+            WebRequest wb = WebRequest.Create(url);
+            WebResponse rp = wb.GetResponse();
+            StreamReader reader = new StreamReader(rp.GetResponseStream(), Encoding.ASCII);
+            Console.WriteLine(reader.ReadToEnd());
+        }
+    }
+}
